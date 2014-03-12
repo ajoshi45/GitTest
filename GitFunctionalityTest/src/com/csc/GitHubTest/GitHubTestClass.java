c2ece50a3b9442ca1e46cf8dd045c83cbc1af11a
ajoshi45 <ajoshi45@csc.com>
2014-03-12 13:46:59 +0530

    Update GitHubTestClass.java

--- GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
+++ GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
@@ -7,6 +7,7 @@ public class GitHubTestClass {
 System.out.println("Test class created");
 System.out.println("New Change122232");
 System.out.println("fetch change");
+System.out.println("changing in github");
 	}
 
 }
