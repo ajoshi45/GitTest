53dabf4def3dc800f27afd3b152b75996a731bed
ajoshi45 <ajoshi45@csc.com>
2014-03-12 18:23:12 +0530

    test class versioning check
    Signed-off-by: ajoshi45 <ajoshi45@csc.com>

---
+++ GitFunctionalityTest/src/com/csc/GitHubTest/TestClass1.java
@@ -0,0 +1,11 @@
+package com.csc.GitHubTest;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new class created to check versioning");
+
+	}
+
+}
