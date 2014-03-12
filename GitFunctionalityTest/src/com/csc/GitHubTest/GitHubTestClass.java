c413d332a77928fedfa6cc537277e29149995db8
ajoshi45 <ajoshi45@csc.com>
2014-03-12 17:14:11 +0530

    Update GitHubTestClass.java

--- GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
+++ GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
@@ -9,6 +9,7 @@ System.out.println("New Change122232");
 System.out.println("fetch change awefrrq2wfa");
 System.out.println("New Branch added");
 System.out.println("changing in githubfhjfkug");
+System.out.println("Change added in the test branch in github ");
 
 	}
 
