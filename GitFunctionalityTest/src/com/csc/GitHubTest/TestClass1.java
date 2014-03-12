42b290ba0f75c70bc0d050627e97ba1d133669dd
ajoshi45 <ajoshi45@csc.com>
2014-03-12 18:26:27 +0530

    Signed-off-by: ajoshi45 <ajoshi45@csc.com>

--- GitFunctionalityTest/src/com/csc/GitHubTest/TestClass1.java
+++ GitFunctionalityTest/src/com/csc/GitHubTest/TestClass1.java
@@ -5,6 +5,7 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new class created to check versioning");
+		System.out.println("new SOP created to check the difference between versions");
 
 	}
 
