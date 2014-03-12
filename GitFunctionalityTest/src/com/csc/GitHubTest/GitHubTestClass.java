ba8f0e870b5ff59f274ff49231bb9ffb354abaf8
ajoshi45 <ajoshi45@csc.com>
2014-03-12 12:52:54 +0530

    Signed-off-by: ajoshi45 <ajoshi45@csc.com>

--- GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
+++ GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
@@ -5,7 +5,7 @@ public class GitHubTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Test class created");
-System.out.println("New Change");
+System.out.println("New Change122232");
 	}
 
 }
