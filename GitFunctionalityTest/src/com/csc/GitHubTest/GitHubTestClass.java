0cf331f5a92af2622b44dca00f4a629914928c9c
ajoshi45 <ajoshi45@csc.com>
2014-03-12 18:18:18 +0530

    Signed-off-by: ajoshi45 <ajoshi45@csc.com>

--- GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
+++ GitFunctionalityTest/src/com/csc/GitHubTest/GitHubTestClass.java
@@ -4,12 +4,7 @@ public class GitHubTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Test class created");
-System.out.println("New Change122232");
-System.out.println("fetch change awefrrq2wfa");
-System.out.println("New Branch added");
-System.out.println("changing in githubfhjfkug");
-System.out.println("Change added in the test branch in github ");
+System.out.println("Test branch version");
 
 	}
 
